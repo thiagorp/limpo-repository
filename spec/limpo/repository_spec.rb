1e79655bab840902a81c9797209dbed7adfb40cd
Thiago Rodrigues de Paula <thiago.rdp@gmail.com>
2017-02-23 21:23:17 +0100

    Fix gemspec and support files

--- spec/limpo/repository_spec.rb
+++ spec/limpo/repository_spec.rb
@@ -4,8 +4,4 @@ describe Limpo::Repository do
   it "has a version number" do
     expect(Limpo::Repository::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
